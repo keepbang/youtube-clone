f738aa254f47017f98a41be73624faf1d81bf218
keepbang <rlqud1125@naver.com>
2021-04-27 19:58:14 +0900

    add Subscriber model

---
+++ server/models/Subscriber.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = require('mongoose');
+
+const subscriberSchema = mongoose.Schema({
+    userTo: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    userFrom: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+
+},{timestamp: true});
+
+
+const Subscriber = mongoose.model('Video', subscriberSchema);
+
+module.exports = { Subscriber }
