784db0b217a566bc24d87167dd8afcee9b3c2bc6
keepbang <rlqud1125@naver.com>
2021-04-28 22:59:28 +0900

    subscription page 개발

--- server/routes/video.js
+++ server/routes/video.js
@@ -6,6 +6,7 @@ const { auth } = require("../middleware/auth");
 const multer = require("multer");
 const path = require("path");
 var ffmpeg = require("fluent-ffmpeg");
+const { Subscriber } = require('../models/Subscriber');
 
 let storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -104,6 +105,31 @@ router.get('/getVideos',(req,res) => {
         .exec((err, videos)=>{
             if(err) return res.status(400).send(err);
             res.status(200).json({success:true, videos})
+        })
+})
+
+router.post('/getSubscriptionVideos',(req,res) => {
+
+    //자신의 아이디를 가지고 구독하는 사람들을 찾는다.
+    Subscriber.find({userFrom: req.body.userFrom})
+        .exec((err, subscriberInfo) => {
+            if(err) return res.status(400).send(err);
+            
+            let subscribedUser = [];
+
+            subscriberInfo.map((subscriber, i) => {
+                subscribedUser.push(subscriber.userTo);
+            })
+
+            //찾은 사람들의 비디오를 가지고 온다.
+
+            Video.find({writer : {$in: subscribedUser}})
+                .populate('writer')
+                .exec((errs, videos) => {
+                    if(errs) return res.status(400).send(errs);
+                    res.status(200).json({success: true, videos})
+                })
+
         })
 })
 
