d2b4fcea4d9efacec23bd5d74592a9b704f1723a
keepbang <rlqud1125@naver.com>
2021-05-05 14:28:52 +0900

    Comment Component 생성

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -3,6 +3,7 @@ import {Row, Col, List, Avatar} from 'antd';
 import Axios from 'axios';
 import SideVideo from "./Sections/SideVideo";
 import Subscribe from "./Sections/Subscribe";
+import Comment from "./Sections/Comment";
 
 function VideoDetailPage(props) {
 
@@ -43,6 +44,10 @@ function VideoDetailPage(props) {
                                 description={VideoDetail.description}
                             />
                         </List.Item>
+
+                        {/* Comments */}
+                        <Comment />
+
                     </div>
                 </Col>
                 <Col lg={6} xs={24}>
