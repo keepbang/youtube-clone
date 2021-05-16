1ed8ac92afe27edd47c08a937c534e49bcb36da4
keepbang <rlqud1125@naver.com>
2021-05-16 22:06:59 +0900

    create like/dislike model

---
+++ server/models/DisLike.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const disLikeSchema = Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    commentId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Comment'
+    },
+    videoId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Video'
+    }
+
+}, { timestamps: true })
+
+
+const DisLike = mongoose.model('DisLike', disLikeSchema);
+
+module.exports = { DisLike }
